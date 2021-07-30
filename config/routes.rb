8094e6f5aa0c2241b5c5d0b3655506213a01a6a2
Felipe Quaresma <felipeequaresma@gmail.com>
2021-07-29 23:21:38 -0300

    improvement routes and add namespace for manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: 'admin', path_names: { sign_in: 'login',
-                                                  sign_out: 'logout',
-                                                  password: 'secret',
-                                                  confirmation: 'verification',
-                                                  unlock: 'unblock',
-                                                  registration: 'register',
-                                                  sign_up: 'cmon_let_me_in' }
+  devise_for :users, path: 'manager', path_names: { sign_in: 'login',
+                                                    sign_out: 'logout',
+                                                    password: 'secret',
+                                                    confirmation: 'verification',
+                                                    unlock: 'unblock',
+                                                    registration: 'register' }
 
   resources :categories, only: %i[index update destroy show create]
   resources :orders, only: %i[index update destroy show create]
   resources :products, only: %i[index update destroy show create]
   resources :employees, only: %i[index update destroy show create]
   resources :product_orders, only: %i[index update destroy show create]
+
+  namespace :manager do
+    get '/' => 'managers#index'
+  end
 
   root to: 'home#index'
 end
