f906cb0fd814c4b60a32dab973ad533fbe9686fb
Felipe Quaresma <felipeequaresma@gmail.com>
2021-07-29 23:25:45 -0300

    improvement application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
+# Application Controller
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || manager_path
+  end
+
+  def after_sign_out_path_for(resource)
+    stored_location_for(resource) || user_session_path
+  end
 end
