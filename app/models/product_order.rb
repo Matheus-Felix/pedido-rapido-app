6a7bfed91df16af7b5acb09c87ff89915d633352
Felipe Quaresma <felipeequaresma@gmail.com>
2021-08-01 00:57:26 -0300

    improvement layout

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,4 +1,8 @@
 class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  enum status: { open: 0, preparing: 1, done: 2, canceled: 3 }
+
+  scope :per_status, ->(status) { where(status: status) }
 end
