857083972c35eebc88e2a5572c44dbc009a2adb4
Felipe Quaresma <felipeequaresma@gmail.com>
2021-08-01 00:58:29 -0300

    improvement employe

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ApplicationRecord
-    has_many :orders
-    enum type: { waiter: 0, cooker: 1, admin: 2}
+  has_many :orders
+  enum worker: { waiter: 0, cooker: 1, admin: 2 }
 end
